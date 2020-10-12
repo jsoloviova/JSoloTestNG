c371ff313161f04c8492962df5b894e9f5f4d074
Julia <iulia.tdl@gmail.com>
2020-10-12 11:12:13 +0300

    Cycle changes

--- src/test/java/SearchResultTest.java
+++ src/test/java/SearchResultTest.java
@@ -23,40 +23,17 @@ public class SearchResultTest extends BaseUiTests {
         driver.findElement(By.name("q")).sendKeys("iphone kyiv buy" + Keys.ENTER);
         wait.until(presenceOfElementLocated(By.cssSelector("#result-stats")));
 
-        try {
-            for (int i = 0; i < 5; i++) {
-                List<WebElement> linksResult = driver.findElements(By.className("iUh30 gBIQub bc tjvcx"));
-                int numberOfLinks = linksResult.size();
-                for (int k = 0; k < numberOfLinks; k++) {
-                    if (linksResult.contains(driver.findElement(By.partialLinkText(linkText)))) {
-                        System.out.println("STYLUS.UA found on " + (i + 1) + " page");
-                        //break;
-                    } else {
-                        driver.findElement(By.id("pnnext")).click();
-                        wait.until(presenceOfElementLocated(By.cssSelector("#result-stats")));
-                    }
-                }
+        for (int i = 0; i < 5; i++) {
+            List<WebElement> linksResult = driver.findElements(By.partialLinkText(linkText));
+            int numberOfLinks = linksResult.size();
+            if (numberOfLinks == 1) {
+                System.out.println("STYLUS.UA found on " + (i + 1) + " page");
                 break;
             }
-        } catch (Exception e) {
-            System.out.println("STYLUS.UA not found on first 5 pages");
+            if (numberOfLinks == 0) {
+                driver.findElement(By.id("pnnext")).click();
+                wait.until(presenceOfElementLocated(By.cssSelector("#result-stats")));
+            }
         }
-
-
-//        try {
-//            for (int i = 0; i < 5; i++) {
-//                if (driver.findElement(By.partialLinkText("mrfix.ua")) != null) {
-//                    assertEquals(driver.findElement(By.partialLinkText("mrfix.ua")), "www.mrfix.ua");
-//                    System.out.println("STYLUS.UA found on " + i+1 + " page");
-//                    break;
-//                } else {
-//                    driver.findElement(By.id("pnnext")).click();
-//                    wait.until(presenceOfElementLocated(By.cssSelector("#result-stats")));
-//                }
-//            }
-//        } catch (Exception e) {
-//            System.out.println("STYLUS.UA not found on first 5 pages");
-//        }
-//        driver.quit();
     }
 }
